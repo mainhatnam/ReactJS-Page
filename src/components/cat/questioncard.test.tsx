10c94f54a7bb3be2c986e2c97fa6ce1e4ed75630
mainhatnam <mainhatnam123@gmail.com>
2025-04-30 02:53:33 +0700

    feat: unit test

---
+++ src/components/cat/questioncard.test.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import {render,screen} from '@testing-library/react'
+import {describe,it,expect} from 'vitest'
+import { CatCard } from "./cardcat";
+
+
+describe("ok nem",() => {
+    it('Nem first test with react js',() => {
+        render(<CatCard description="day la con meo" image="https://cdn2.thecatapi.com/images/bsd.jpg" name="meo anh long ngan" />)
+        expect(screen.getByText("day la con meo")).toBeInTheDocument()
+    })
+})
