c62bcf2bc2a63ab603202b6a842573eb8590afe6
mainhatnam <mainhatnam123@gmail.com>
2025-04-30 04:01:50 +0700

    update

--- src/components/cat/questioncard.test.tsx
+++ src/components/cat/questioncard.test.tsx
@@ -6,6 +6,6 @@ import { CatCard } from "./cardcat";
 describe("ok nem",() => {
     it('Nem first test with react js',() => {
         render(<CatCard description="day la con meo" image="https://cdn2.thecatapi.com/images/bsd.jpg" name="meo anh long ngan" />)
-        expect(screen.getByText("day la con meo1")).toBeInTheDocument()
+        expect(screen.getByText("day la con meo")).toBeInTheDocument()
     })
 })
